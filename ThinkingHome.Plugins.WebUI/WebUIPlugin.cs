60e9eeea230c627750cbb6c0878a47336955524e
dima117 <dima117a@gmail.com>
2014-04-18 22:52:35 +0400

    - к UI плагину подключен jquery и bootstrap;
    - заготовка для стартовой страницы;

--- ThinkingHome.Plugins.WebUI/WebUIPlugin.cs
+++ ThinkingHome.Plugins.WebUI/WebUIPlugin.cs
@@ -4,8 +4,26 @@ using ThinkingHome.Plugins.Listener.Handlers;
 namespace ThinkingHome.Plugins.WebUI
 {
 	[Plugin]
+
+	// html
 	[HttpResource("/", "ThinkingHome.Plugins.WebUI.Resources.Index.html", "text/html")]
-    public class WebUIPlugin : Plugin
-    {
-    }
+	
+	// js
+	[HttpResource("/js/jquery.min.js", "ThinkingHome.Plugins.WebUI.Resources.js.jquery.min.js", "text/javascript")]
+	[HttpResource("/js/bootstrap.min.js", "ThinkingHome.Plugins.WebUI.Resources.js.bootstrap.min.js", "text/javascript")]
+
+	// css
+	[HttpResource("/css/bootstrap-theme.min.css", "ThinkingHome.Plugins.WebUI.Resources.css.bootstrap-theme.min.css", "text/css")]
+	[HttpResource("/css/bootstrap.min.css", "ThinkingHome.Plugins.WebUI.Resources.css.bootstrap.min.css", "text/css")]
+
+	// fonts
+	[HttpResource("/fonts/glyphicons-halflings-regular/.eot", "ThinkingHome.Plugins.WebUI.Resources.css.glyphicons-halflings-regular.eot", "application/vnd.ms-fontobject")]
+	[HttpResource("/fonts/glyphicons-halflings-regular/.svg", "ThinkingHome.Plugins.WebUI.Resources.css.glyphicons-halflings-regular.svg", "image/svg+xml")]
+	[HttpResource("/fonts/glyphicons-halflings-regular/.ttf", "ThinkingHome.Plugins.WebUI.Resources.css.glyphicons-halflings-regular.ttf", "application/x-font-truetype")]
+	[HttpResource("/fonts/glyphicons-halflings-regular/.woff", "ThinkingHome.Plugins.WebUI.Resources.css.glyphicons-halflings-regular.woff", "application/font-woff")]
+
+
+	public class WebUIPlugin : Plugin
+	{
+	}
 }
