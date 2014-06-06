9a17342cbb7e3b52b0a1333c788f9f27a97d430a
dima117 <dima117a@gmail.com>
2014-06-07 02:34:41 +0400

    - передача номера билда через параметр при автосборке;

--- Setup/build-installer/build-msi.bat
+++ Setup/build-installer/build-msi.bat
@@ -1,4 +1,4 @@
 rd  /s /q ..\dist
 rd /s /q ..\msi
-nant -buildfile:main.build 
+nant -buildfile:main.build  -D:build.number=12
 pause
--- Setup/build-installer/build-site.bat
+++
@@ -1,4 +0,0 @@
-rd  /s /q ..\dist
-rd /s /q ..\msi
-nant -buildfile:site.build 
-pause
