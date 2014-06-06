06e7d8c54b51bd38536fbb64ad86fee52f203f3e
dima117 <dima117a@gmail.com>
2014-06-06 23:42:23 +0400

    - централизованное назначение номера версии;

---
+++ AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright ©  2013-2014")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: ComVisible(false)]
