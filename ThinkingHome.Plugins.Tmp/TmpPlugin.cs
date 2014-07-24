2e326ba04c7b1de3d1e708ab162fc9de52d70c71
dima117 <dima117a@gmail.com>
2014-07-24 18:29:07 +0400

    - удалена "левая" папка data-access, которая раньше использовалась при сборке инсталлятора;
    - удалены "левые проекты";

--- ThinkingHome.Plugins.Tmp/TmpPlugin.cs
+++ ThinkingHome.Plugins.Tmp/TmpPlugin.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Net;
 using System.Net.Mail;
 using System.Threading;
 using ThinkingHome.Core.Plugins;
 using ThinkingHome.Plugins.AlarmClock;
 using ThinkingHome.Plugins.Audio;
 using ThinkingHome.Plugins.Audio.Internal;
-using ThinkingHome.Plugins.DlinkDCS930L;
 using ThinkingHome.Plugins.Scripts;
 
 namespace ThinkingHome.Plugins.Tmp
@@ -18,13 +19,38 @@ namespace ThinkingHome.Plugins.Tmp
 		[ScriptCommand("sendDCS930Image")]
 		public void SendImage(string to, string subject)
 		{
-			var bytes = Context.GetPlugin<DlinkDCS930LPlugin>().GetImage();
+			var cameraIp = ConfigurationManager.AppSettings["testCameraIP"];
+			var bytes = GetImage(cameraIp, "admin", "");
 
 			var fileName = string.Format("{0:yyyyMMddHHmmss}.jpg", DateTime.Now);
 			var path = Path.Combine(@"D:\", fileName);
 			File.WriteAllBytes(path, bytes);
 
 			Send(to, subject, fileName, bytes);
+		}
+
+		protected static byte[] GetImage(string ip, string login, string password)
+		{
+			string url = string.Format(@"http://{0}/image/jpeg.cgi", ip);
+			WebRequest request = WebRequest.Create(url);
+			request.Credentials = new NetworkCredential(login, password);
+			request.PreAuthenticate = true;
+
+			using (var response = request.GetResponse())
+			{
+				using (var stream = response.GetResponseStream())
+				{
+					if (stream == null)
+					{
+						return new byte[0];
+					}
+
+					var ms = new MemoryStream();
+					stream.CopyTo(ms);
+					byte[] data = ms.ToArray();
+					return data;
+				}
+			}
 		}
 
 		#endregion
