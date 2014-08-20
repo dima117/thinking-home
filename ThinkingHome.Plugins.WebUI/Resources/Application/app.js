2dae7eff52c691c3e98291784ab452b3f8e33960
dima117 <dima117a@gmail.com>
2014-08-20 21:50:01 +0400

    - обработка параметров в URL (требуется рефакторинг);

--- ThinkingHome.Plugins.WebUI/Resources/Application/app.js
+++ ThinkingHome.Plugins.WebUI/Resources/Application/app.js
@@ -6,23 +6,10 @@
 		regionContent: "#region-page-content"
 	});
 
-	app.setContentView = function (view) {
+	app.setContentView = function(view) {
 		app.regionContent.show(view);
-	},
-
-	app.navigate = function (route) {
-
-		if (route) {
-
-			var args = Array.prototype.slice.call(arguments, 1);
-
-			require([route], function (obj) {
-
-				obj.start.apply(obj, args);
-				backbone.history.navigate(route);
-			});
-		}
 	};
+
 
 	app.addTile = function (def, options) {
 
@@ -34,11 +21,41 @@
 			});
 	};
 
-	app.router = new marionette.AppRouter({
-		appRoutes: { '*path': 'loadPage' },
-		controller: { loadPage: app.navigate }
-	});
+	app.navigate = function (route) {
 
+		if (route) {
+
+			var args = Array.prototype.slice.call(arguments, 1);
+
+			require([route], function (obj) {
+
+				obj.start.apply(obj, args);
+
+				if (args && args.length) {
+					//encodeURIComponent('edg/fwegw/e&wegewg/we/g')
+					route += '?' + args.join('/');
+				}
+
+				backbone.history.navigate(route);
+			});
+		}
+	};
+
+	app.router = new marionette.AppRouter({
+		appRoutes: { '*path(?*args)': 'loadPage' },
+		controller: {
+			loadPage: function (route, args) {
+
+				//decodeURIComponent("edg%2Ffwegw%2Fe%26wegewg%2Fwe%2Fg");
+				var x = args === null || args === undefined
+					? [route]
+					: [route].concat(args.split('/'));;
+
+				app.navigate.apply(this, x);
+			}
+		}
+	});
+	
 	app.on('page:load', app.navigate);
 
 	app.on('initialize:after', function () {
