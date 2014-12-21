6cc64ad5c1c3f04e64b170b43dc39b286dbc08d7
khapalov <khapalov@outlook.com>
2014-12-21 18:13:15 +0300

    OneWire plugin

---
+++ ThinkingHome.Plugins.OneWire/OneWirePluggin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.OneWire
+{
+    [Plugin]
+    public class OneWirePluggin : PluginBase 
+    {        
+        public override void InitPlugin()
+        {
+            Debugger.Launch();
+            base.InitPlugin();
+        }
+
+        public override void StartPlugin()
+        {
+            Logger.Info("Start Plugin OneWire");
+            base.StartPlugin();
+        }
+
+        public override void StopPlugin()
+        {
+            base.StopPlugin();            
+        }
+    }
+}
