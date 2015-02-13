7c73ec706fd9538c0bafac01fad7a2868f851989
dima117 <dima117a@gmail.com>
2015-02-13 09:02:23 +0300

    - alarm editor переписан без использования marionette modules;

--- ThinkingHome.Plugins.AlarmClock/Resources/alarm-editor.js
+++ ThinkingHome.Plugins.AlarmClock/Resources/alarm-editor.js
@@ -1,42 +1,37 @@
-﻿define(
-	['app',
+﻿define(['app',
 		'webapp/alarm-clock/editor-model',
 		'webapp/alarm-clock/editor-view'],
 	function (application, models, views) {
 
-		application.module('AlarmClock.Editor', function (module, app, backbone, marionette, $, _) {
+		var api = {
+			createEditor: function (model) {
 
-			var api = {
-				createEditor: function (model) {
+				var view = new views.AlarmEditorView({ model: model });
 
-					var view = new views.AlarmEditorView({ model: model });
+				view.on('alarm-clock:editor:save', api.save);
+				view.on('alarm-clock:editor:cancel', api.redirectToList);
 
-					view.on('alarm-clock:editor:save', api.save);
-					view.on('alarm-clock:editor:cancel', api.redirectToList);
+				application.setContentView(view);
+			},
 
-					app.setContentView(view);
-				},
-				
-				edit: function (id) {
-					
-					models.loadEditorData(id).done(api.createEditor);
-				},
+			edit: function (id) {
 
-				save: function () {
+				models.loadEditorData(id).done(api.createEditor);
+			},
 
-					var model = this.model;
-					models.saveAlarm(model).done(api.redirectToList);
-				},
-				redirectToList: function () {
-					app.navigate('webapp/alarm-clock/list');
-				}
-			};
+			save: function () {
 
-			module.start = function (id) {
+				var model = this.model;
+				models.saveAlarm(model).done(api.redirectToList);
+			},
+			redirectToList: function () {
+				application.navigate('webapp/alarm-clock/list');
+			}
+		};
+
+		return {
+			start: function (id) {
 				api.edit(id);
-			};
-
-		});
-
-		return application.AlarmClock.Editor;
+			}
+		};
 	});
