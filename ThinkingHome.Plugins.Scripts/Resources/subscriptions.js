4e619d1c21f0478226f2d1b1e59c042ce2ff240e
dima117 <dima117a@gmail.com>
2015-03-01 12:03:39 +0300

    - переписан модуль subscription list (main) без использования модулей marionette.js;

--- ThinkingHome.Plugins.Scripts/Resources/subscriptions.js
+++ ThinkingHome.Plugins.Scripts/Resources/subscriptions.js
@@ -2,68 +2,64 @@
 	['app', 'common',
 		'webapp/scripts/subscriptions-model',
 		'webapp/scripts/subscriptions-view'],
-	function (application, commonModule, models, views) {
+	function (application, common, models, views) {
 
-		application.module('Scripts.Subscriptions', function (module, app, backbone, marionette, $, _) {
+		var layoutView;
 
-			var layoutView;
+		var api = {
 
-			var api = {
+			addSubscription: function () {
 
-				addSubscription: function () {
+				var eventAlias = this.model.get('selectedEventAlias');
+				var scriptId = this.model.get('selectedScriptId');
 
-					var eventAlias = this.model.get('selectedEventAlias');
-					var scriptId = this.model.get('selectedScriptId');
+				models.addSubscription(eventAlias, scriptId).done(api.reloadList);
+			},
 
-					models.addSubscription(eventAlias, scriptId).done(api.reloadList);
-				},
+			deleteSubscription: function (childView) {
 
-				deleteSubscription: function (childView) {
+				var eventAlias = childView.model.get('eventAlias');
+				var scriptName = childView.model.get('scriptName');
 
-					var eventAlias = childView.model.get('eventAlias');
-					var scriptName = childView.model.get('scriptName');
+				if (common.utils.confirm('Delete the subscription?\n- event: "{0}"\n- script: "{1}"', eventAlias, scriptName)) {
 
-					if (commonModule.utils.confirm('Delete the subscription?\n- event: "{0}"\n- script: "{1}"', eventAlias, scriptName)) {
-
-						var subscriptionId = childView.model.get('id');
-						models.deleteSubscription(subscriptionId).done(api.reloadList);
-					}
-				},
-
-				reloadList: function () {
-
-					models.loadSubscriptions()
-						.done(function (list) {
-
-							var view = new views.SubscriptionListView({ collection: list });
-							view.on('childview:scripts:subscription:delete', api.deleteSubscription);
-							layoutView.regionList.show(view);
-						});
-				},
-
-				reloadForm: function () {
-
-					models.loadFormData()
-						.done(function (formData) {
-
-							var form = new views.SubscriptionFormView({ model: formData });
-							form.on('scripts:subscription:add', api.addSubscription);
-							layoutView.regionForm.show(form);
-						});
+					var subscriptionId = childView.model.get('id');
+					models.deleteSubscription(subscriptionId).done(api.reloadList);
 				}
-			};
+			},
 
-			module.start = function () {
+			reloadList: function () {
+
+				models.loadSubscriptions()
+					.done(function (list) {
+
+						var view = new views.SubscriptionListView({ collection: list });
+						view.on('childview:scripts:subscription:delete', api.deleteSubscription);
+						layoutView.regionList.show(view);
+					});
+			},
+
+			reloadForm: function () {
+
+				models.loadFormData()
+					.done(function (formData) {
+
+						var form = new views.SubscriptionFormView({ model: formData });
+						form.on('scripts:subscription:add', api.addSubscription);
+						layoutView.regionForm.show(form);
+					});
+			}
+		};
+
+		return {
+			start: function () {
 
 				// init layout
 				layoutView = new views.SubscriptionLayout();
-				app.setContentView(layoutView);
+				application.setContentView(layoutView);
 
 				api.reloadForm();
 				api.reloadList();
-			};
-
-		});
-
-		return application.Scripts.Subscriptions;
+			}
+		};
 	});
