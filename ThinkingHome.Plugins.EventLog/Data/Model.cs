363c3ef1a49002ba6241f52681a32f52a0eda2ad
dima117 <dima117a@gmail.com>
2015-03-18 22:00:04 +0300

    - заготовка плагина для работы с событиями;

---
+++ ThinkingHome.Plugins.EventLog/Data/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkingHome.Plugins.EventLog.Data
+{
+	public class EventLogItem
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual DateTime Timestamp { get; set; }
+	}
+}
---
+++ ThinkingHome.Plugins.EventLog/EventLogPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping.ByCode;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.EventLog
+{
+	[Plugin]
+	public class EventLogPlugin : PluginBase
+    {
+		// todo: заменить AssemblyInfo
+		public override void InitDbModel(ModelMapper mapper)
+		{
+			base.InitDbModel(mapper);
+		}
+    }
+}
