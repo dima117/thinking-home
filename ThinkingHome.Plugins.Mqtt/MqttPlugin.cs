e55b418a6f8a91e06921f84cf4ddde06e605a5ff
dima117 <dima117a@gmail.com>
2015-05-18 08:59:57 +0300

    - заготовка плагина MQTT;

---
+++ ThinkingHome.Plugins.Mqtt/MqttPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.Mqtt
+{
+	[Plugin]
+    public class MqttPlugin : PluginBase
+    {
+		
+    }
+}
