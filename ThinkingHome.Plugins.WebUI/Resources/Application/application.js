809933e3f64f66d4a30089b7d2c22c3b37fa0b23
dima117 <dima117a@gmail.com>
2015-06-17 22:11:03 +0300

    - удалены неиспользуемые стили;
    - начат рефакторинг инфраструктуры webui;

---
+++ ThinkingHome.Plugins.WebUI/Resources/Application/application.js
@@ -0,0 +1,22 @@
+﻿define(['lib'], function(lib, router, layout) {
+
+	var app = new lib.marionette.Application();
+
+	app.router = new router();
+	app.layout = new layout();
+
+	app.navigate = function(path) {
+		
+	};
+
+	app.router.on('navigate', app.navigate);
+	app.layout.on('navigate', app.navigate);
+
+	app.on('start', function () {
+
+		app.layout.render();
+		app.router.start();
+	});
+
+	return app;
+});
