7cb7d1686cdba9ed8a47fe4615d93a718981570f
dima117 <dima117a@gmail.com>
2015-06-27 20:56:36 +0300

    - API для получения списка виджетов с данными;

--- ThinkingHome.Plugins.UniUI/UniUiPlugin.Dashboard.cs
+++ ThinkingHome.Plugins.UniUI/UniUiPlugin.Dashboard.cs
@@ -1,4 +1,13 @@
-﻿using ThinkingHome.Plugins.Listener.Attributes;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using NHibernate.Linq;
+using ThinkingHome.Core.Plugins.Utils;
+using ThinkingHome.Plugins.Listener.Api;
+using ThinkingHome.Plugins.Listener.Attributes;
+using ThinkingHome.Plugins.UniUI.Model;
+using ThinkingHome.Plugins.UniUI.Widgets;
 using ThinkingHome.Plugins.WebUI.Attributes;
 
 namespace ThinkingHome.Plugins.UniUI
@@ -10,6 +19,75 @@ namespace ThinkingHome.Plugins.UniUI
 
 	public partial class UniUiPlugin
 	{
+		[HttpCommand("/api/uniui/dashboard/details")]
+		public object GetDdashboardDetails(HttpRequestParams request)
+		{
+			Guid dashboardId = request.GetRequiredGuid("id");
 
+			using (var session = Context.OpenSession())
+			{
+				var allWidgets = session.Query<Widget>()
+						.Where(w => w.Dashboard.Id == dashboardId)
+						.ToArray();
+
+				var allParameters = session.Query<WidgetParameter>()
+						.Where(w => w.Widget.Dashboard.Id == dashboardId)
+						.ToArray();
+
+				var list = GetWidgetListModel(allWidgets, allParameters, session);
+
+				return list;
+			}
+		}
+
+		private object[] GetWidgetListModel(Widget[] allWidgets, WidgetParameter[] allParameters, ISession session)
+		{
+			var list = new List<object>();
+
+			foreach (var widget in allWidgets)
+			{
+				var def = defs.GetValueOrDefault(widget.TypeAlias);
+
+				if (def != null)
+				{
+					var parameters = allParameters.Where(p => p.Widget.Id == widget.Id).ToArray();
+
+					var model = GetWidgetModel(def, widget, parameters, session);
+
+					if (model != null)
+					{
+						list.Add(model);
+					}
+				}
+			}
+			return list.ToArray(); ;
+		}
+
+		private object GetWidgetModel(IWidgetDefinition def, Widget widget, WidgetParameter[] parameters, ISession session)
+		{
+			try
+			{
+				var data = def.GetWidgetData(widget, parameters, session, Logger);
+
+				var model = new
+				{
+					id = widget.Id,
+					type = widget.TypeAlias,
+					displayName = widget.DisplayName,
+					sortOrder = widget.SortOrder,
+					data
+				};
+
+				return model;
+			}
+			catch (Exception ex)
+			{
+				var message = string.Format(
+					"UI widget error (id: {0}, type:{1})", widget.Id, widget.TypeAlias);
+				Logger.ErrorException(message, ex);
+
+				return null;
+			}
+		}
 	}
 }
