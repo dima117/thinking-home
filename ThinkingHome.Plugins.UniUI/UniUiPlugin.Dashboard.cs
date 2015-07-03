2a1acee7e32367122b76dcad8518a415824904a1
dima117 <dima117a@gmail.com>
2015-07-03 22:39:27 +0300

    merge UniUiPlugin.DashboardList.cs => UniUiPlugin.Dashboard.cs;
    dashboard-model

--- ThinkingHome.Plugins.UniUI/UniUiPlugin.Dashboard.cs
+++ ThinkingHome.Plugins.UniUI/UniUiPlugin.Dashboard.cs
@@ -13,6 +13,66 @@ namespace ThinkingHome.Plugins.UniUI
 {
 	public partial class UniUiPlugin
 	{
+		[HttpCommand("/api/uniui/dashboard/list")]
+		public object GetDashboardList(HttpRequestParams request)
+		{
+			using (var session = Context.OpenSession())
+			{
+				var list = session.Query<Dashboard>().ToList();
+
+				var model = list
+					.Select(x => new
+					{
+						id = x.Id,
+						title = x.Title,
+						sortOrder = x.SortOrder
+					})
+					.ToList();
+
+				return model;
+			}
+		}
+
+		[HttpCommand("/api/uniui/dashboard/create")]
+		public object CreateDashboard(HttpRequestParams request)
+		{
+			string title = request.GetRequiredString("title");
+
+			using (var session = Context.OpenSession())
+			{
+				var dashboard = new Dashboard
+				{
+					Id = Guid.NewGuid(),
+					Title = title,
+					SortOrder = int.MaxValue
+				};
+
+				session.Save(dashboard);
+				session.Flush();
+
+				return dashboard.Id;
+			}
+		}
+
+		[HttpCommand("/api/uniui/dashboard/delete")]
+		public object DeleteDashboard(HttpRequestParams request)
+		{
+			Guid id = request.GetRequiredGuid("id");
+
+			using (var session = Context.OpenSession())
+			{
+				var dashboard = session.Get<Dashboard>(id);
+
+				if (dashboard != null)
+				{
+					session.Delete(dashboard);
+					session.Flush();
+				}
+			}
+
+			return null;
+		}
+
 		[HttpCommand("/api/uniui/dashboard/details")]
 		public object GetDashboardDetails(HttpRequestParams request)
 		{
--- ThinkingHome.Plugins.WebUI/Resources/Application/widgets/dashboard-model.js
+++ ThinkingHome.Plugins.WebUI/Resources/Application/widgets/dashboard-model.js
@@ -2,12 +2,42 @@
 	['lib'],
 	function (lib) {
 
+		var dashboardListItem = lib.backbone.Model.extend({
+			defaults: {
+				active: false
+			}
+		});
+
+		var dashboardList = lib.backbone.Collection.extend({
+			model: dashboardListItem,
+			comparator: 'sortOrder'
+		});
+
 		var api = {
-			loadDashboardList: function() {
-				
+			loadDashboardList: function () {
+
 				var defer = lib.$.Deferred();
 
 				lib.$.getJSON('/api/uniui/dashboard/list')
+					.done(function (data) {
+
+						var list = new dashboardList(data);
+
+						defer.resolve(list);
+					})
+					.fail(function () {
+
+						defer.resolve(undefined);
+					});
+
+				return defer.promise();
+			},
+
+			loadDashboardDetails: function (id) {
+
+				var defer = lib.$.Deferred();
+
+				lib.$.getJSON('/api/uniui/dashboard/details', { id: id })
 					.done(function (data) {
 
 						var list = new lib.backbone.Collection(data);
