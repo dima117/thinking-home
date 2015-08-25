b6fdd409058c76d8ff90636defb48400ae941dee
Вячеслав <gorosvia@gmail.com>
2015-08-26 02:36:53 +0400

    New NooUI plugin
    - Создан корректный (вроде) проект плагина.

---
+++ ThinkingHome.Plugins.NooUI/NooUIPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using ThinkingHome.Core.Plugins;
+
+namespace ThinkingHome.Plugins.NooUI
+{
+    class NooUIPlugin
+    {
+    }
+}
