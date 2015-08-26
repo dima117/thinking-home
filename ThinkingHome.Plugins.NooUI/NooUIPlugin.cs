759ac0618a255af4209baf5fc3acaf7d119dd5a5
Вячеслав <gorosvia@gmail.com>
2015-08-26 20:20:22 +0400

    New project

---
+++ ThinkingHome.Plugins.NooUI/NooUIPlugin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasksusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using ThinkingHome.Core.Plugins;
+using ThinkingHome.Core.Plugins.Utils;
+using ThinkingHome.Plugins.UniUI.Model;
+using ThinkingHome.Plugins.UniUI.Widgets;
+using ThinkingHome.Plugins.WebUI.Attributes;
+using NHibernate;
+using NLog;
+
+namespace ThinkingHome.Plugins.NooUI
+{
+    [Plugin]
+   // [WebWidget("NooUISwitcher", "/widgets/NooUISwitcherWidget.js", "ThinkingHome.Plugins.NooUI.Resources.ui.NooUISwitherWidget.js")]
+    class NooUIPlugin : PluginBase
+    {
+
+    }
+}
