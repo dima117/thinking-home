5ba04a0eef1ca9c86939266a582ba4aa07b76122
Вячеслав <gorosvia@gmail.com>
2015-08-26 17:38:13 +0400

    Заготовка виджета включения-выключения

--- ThinkingHome.Plugins.NooUI/NooUIPlugin.cs
+++ ThinkingHome.Plugins.NooUI/NooUIPlugin.cs
@@ -5,10 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.Composition;
 using ThinkingHome.Core.Plugins;
+using ThinkingHome.Core.Plugins.Utils;
+using ThinkingHome.Plugins.UniUI.Model;
+using ThinkingHome.Plugins.UniUI.Widgets;
+using ThinkingHome.Plugins.WebUI.Attributes;
+using NHibernate;
+using NLog;
 
 namespace ThinkingHome.Plugins.NooUI
 {
-    class NooUIPlugin
+    [Plugin]
+    [WebWidget("NooUISwitcher", "/widgets/NooUISwitcherWidget.js", "ThinkingHome.Plugins.NooUI.Resources.ui.NooUISwitherWidget.js")]
+    class NooUIPlugin : PluginBase
     {
+
     }
+
+    
+        [Widget("NooUISwitherWidget")]
+        public class NooUISwitcherWidget : IWidgetDefinition
+        {
+            public string DisplayName
+            {
+                get { return "nooLite (ON/OFF)"; }
+            }
+
+            public object GetWidgetData(Widget widget, WidgetParameter[] parameters, ISession session, Logger logger)
+            {
+                return null;
+            }
+
+            public WidgetParameterMetaData[] GetWidgetMetaData(ISession session, Logger logger)
+            {
+                var fldChannel = new WidgetParameterMetaData
+                {
+                    DisplayName = "Channel",
+                    Name = "channel",
+                    Type = WidgetParameterType.Int32,
+                    Items = new WidgetSelectItem[32]
+                };
+
+                for (var i = 0; i < 32; i++)
+                {
+                    fldChannel.Items[i] = new WidgetSelectItem(i, i.ToString());
+                }
+
+                return new[] { fldChannel };
+            }
+        }/**/
 }
---
+++ ThinkingHome.Plugins.NooUI/Resources/ui/NooUISwitcherWidget.js
@@ -0,0 +1,18 @@
+﻿define(['lib'], function (lib) {
+
+	var emptyWidgetView = lib.marionette.ItemView.extend({
+		template: lib.handlebars.compile(
+			'<div class="th-widget-block th-pointer btn-primary">On</div>' +
+			'<div class="th-widget-block th-pointer btn-primary">Off</div>'),
+		className: 'th-widget-container'
+	});
+
+	return {
+		show: function (model, region) {
+
+			var view = new emptyWidgetView({ model: model });
+
+			region.show(view);
+		}
+	};
+});
