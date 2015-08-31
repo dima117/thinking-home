500c1e52919d914f57f584159953d467a70d1da5
Вячеслав <gorosvia@gmail.com>
2015-09-01 02:44:02 +0300

    Создан виджет выключателя
    Не работает пока

---
+++ ThinkingHome.Plugins.NooUI/Resources/ui/NooUISwitcherWidget.js
@@ -0,0 +1,28 @@
+﻿define(['lib'], function (lib) {
+
+	var nooUISwitcherWidgetView = lib.marionette.ItemView.extend({
+		template: lib.handlebars.compile(
+			'<div class="th-widget-block th-pointer btn-primary">' +
+				'<div class="th-widget-block-title">{{displayName}}</div>' +
+				'<div class="th-widget-block-content">' +
+					'<a href="#on" class="btn btn-default">ON</a>' +
+					'<a href="#off" class="btn btn-default">OFF</a>' +
+				'</div>' +
+			'</div>'),
+		className: 'th-widget-container',
+		triggers: {
+			"click .btn": "btn:click"
+		}
+	});
+
+
+	return {
+		show: function (model, region) {
+
+			var view = new nooUISwitcherWidgetView({ model: model });
+			view.on("btn:click", clickHandler);
+
+			region.show(view);
+		}
+	};
+});
