1676618744c6d79fd7675858313a1f4d72464363
dima117 <dima117a@gmail.com>
2015-10-15 22:59:19 +0300

    - адаптирована страница настроек датчиков микроклимата

--- ThinkingHome.Plugins.Microclimate/Resources/settings.js
+++ ThinkingHome.Plugins.Microclimate/Resources/settings.js
@@ -1,55 +1,56 @@
-﻿define(['app', 'lib',
+﻿define(['lib',
 		'webapp/microclimate/settings-model',
 		'webapp/microclimate/settings-view'],
-	function (application, lib, models, views) {
+	function (lib, models, views) {
 
-		var api = {
-			addSensor: function () {
+		var microclimateSettings = lib.common.AppSection.extend({
+			start: function () {
+				this.loadSettings();
+			},
 
-				var displayName = this.ui.displayName.val();
-				var channel = this.ui.channel.val();
-				var showHumidity = this.ui.showHumidity.prop('checked');
+			addSensor: function (view) {
+
+				var displayName = view.ui.displayName.val();
+				var channel = view.ui.channel.val();
+				var showHumidity = view.ui.showHumidity.prop('checked');
 
 				if (displayName) {
-
 					models.addSensor(displayName, channel, showHumidity)
-						.done(api.loadSettings);
+						.done(this.bind('loadSettings'));
 				}
 			},
-			showSensorDetails: function(view) {
-				
-				var sensorId = view.model.get('id');
-				application.navigate('webapp/microclimate/details', sensorId);
-			},
-			deleteSensor: function (childView) {
 
+			showSensorDetails: function (view) {
+				var sensorId = view.model.get('id');
+				this.application.navigate('webapp/microclimate/details', sensorId);
+			},
+
+			deleteSensor: function (childView) {
 				var displayName = childView.model.get('displayName');
 
 				if (lib.utils.confirm('Delete the sensor "{0}" and all related data?', displayName)) {
-
 					var id = childView.model.get('id');
 
-					models.deleteSensor(id).done(api.loadSettings);
+					models.deleteSensor(id).done(this.bind('loadSettings'));
 				}
 			},
+
+			displaySettings: function (collection) {
+				var view = new views.SensorTable({
+					collection: collection
+				});
+
+				this.listenTo(view, 'add:sensor', this.bind('addSensor', view));
+				this.listenTo(view, 'childview:delete:sensor', this.bind('deleteSensor'));
+				this.listenTo(view, 'childview:show:sensor:details', this.bind('showSensorDetails'));
+
+				this.application.setContentView(view);
+			},
+
 			loadSettings: function () {
-
-				models.loadSensorTable()
-					.done(function (collection) {
-
-						var view = new views.SensorTable({
-							collection: collection
-						});
-
-						view.on('add:sensor', api.addSensor);
-						view.on('childview:delete:sensor', api.deleteSensor);
-						view.on('childview:show:sensor:details', api.showSensorDetails);
-						application.setContentView(view);
-					});
+				models.loadSensorTable().done(this.bind('displaySettings'));
 			}
-		};
+		});
 
-		return {
-			start: api.loadSettings
-		};
+		return microclimateSettings;
 	});
