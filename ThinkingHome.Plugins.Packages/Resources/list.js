a4c6e4e346d95921586a9444f15df9dc03e02e57
dima117 <dima117a@gmail.com>
2015-10-19 00:35:13 +0300

    - адаптирован список установленных  плагинов;

--- ThinkingHome.Plugins.Packages/Resources/list.js
+++ ThinkingHome.Plugins.Packages/Resources/list.js
@@ -1,39 +1,35 @@
 ﻿define(
-	['app', 'webapp/packages/list-model', 'webapp/packages/list-view'],
-	function (application, models, views) {
+	['lib', 'webapp/packages/list-model', 'webapp/packages/list-view'],
+	function (lib, models, views) {
 
-		var api = {
+		var packageList = lib.common.AppSection.extend({
+			start: function () {
+				this.reload();
+			},
 
 			install: function (childView) {
-
 				var packageId = childView.model.get('id');
-				models.installPackage(packageId).done(api.reload);
+				models.installPackage(packageId).done(this.bind('reload'));
 			},
 
 			uninstall: function (childView) {
-
 				var packageId = childView.model.get('id');
-				models.uninstallPackage(packageId).done(api.reload);
+				models.uninstallPackage(packageId).done(this.bind('reload'));
+			},
+
+			displayList: function (items) {
+				var view = new views.PackageListView({ collection: items });
+
+				this.listenTo(view, 'childview:packages:install', this.bind('install'));
+				this.listenTo(view, 'childview:packages:uninstall', this.bind('uninstall'));
+
+				this.application.setContentView(view);
 			},
 
 			reload: function () {
-
-				models.loadPackages()
-					.done(function (items) {
-
-						var view = new views.PackageListView({ collection: items });
-
-						view.on('childview:packages:install', api.install);
-						view.on('childview:packages:uninstall', api.uninstall);
-
-						application.setContentView(view);
-					});
+				models.loadPackages().done(this.bind('displayList'));
 			}
-		};
+		});
 
-		return {
-			start: function () {
-				api.reload();
-			}
-		};
+		return packageList;
 	});
