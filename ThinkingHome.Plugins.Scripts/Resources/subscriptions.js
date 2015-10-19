bc673f1e575b0a21c0883cb1853d8a21862a05bf
dima117 <dima117a@gmail.com>
2015-10-19 22:10:13 +0300

    - адаптирована страница скриптовых событий;
    - исправлены стили для главного меню и полноэкранного режима редактора сценарев;

--- ThinkingHome.Plugins.Scripts/Resources/subscriptions.js
+++ ThinkingHome.Plugins.Scripts/Resources/subscriptions.js
@@ -1,63 +1,54 @@
 ﻿define(
-	['app', 'lib',
+	['lib',
 		'webapp/scripts/subscriptions-model',
 		'webapp/scripts/subscriptions-view'],
-	function (application, lib, models, views) {
+	function (lib, models, views) {
 
-		var layoutView;
+		var subscriptionList = lib.common.AppSection.extend({
+			start: function () {
+				// init layout
+				var layout = this.layout = new views.SubscriptionLayout();
+				this.application.setContentView(layout);
 
-		var api = {
-
-			addSubscription: function (data) {
-
-				models.addSubscription(data.selectedEventAlias, data.selectedScriptId)
-					.done(api.reloadList);
+				this.reloadForm();
+				this.reloadList();
 			},
 
+			addSubscription: function (data) {
+				models.addSubscription(data.selectedEventAlias, data.selectedScriptId)
+					.done(this.bind('reloadList'));
+			},
+				
 			deleteSubscription: function (childView) {
-
 				var eventAlias = childView.model.get('eventAlias');
 				var scriptName = childView.model.get('scriptName');
 
 				if (lib.utils.confirm('Delete the subscription?\n- event: "{0}"\n- script: "{1}"', eventAlias, scriptName)) {
-
 					var subscriptionId = childView.model.get('id');
-					models.deleteSubscription(subscriptionId).done(api.reloadList);
+					models.deleteSubscription(subscriptionId).done(this.bind('reloadList'));
 				}
 			},
 
+			displayList: function (list) {
+				var view = new views.SubscriptionListView({ collection: list });
+				this.listenTo(view, 'childview:scripts:subscription:delete', this.bind('deleteSubscription'));
+				this.layout.regionList.show(view);
+			},
+
 			reloadList: function () {
+				models.loadSubscriptions().done(this.bind('displayList'));
+			},
 
-				models.loadSubscriptions()
-					.done(function (list) {
-
-						var view = new views.SubscriptionListView({ collection: list });
-						view.on('childview:scripts:subscription:delete', api.deleteSubscription);
-						layoutView.regionList.show(view);
-					});
+			displayForm: function (formData) {
+				var form = new views.SubscriptionFormView({ model: formData });
+				this.listenTo(form, 'scripts:subscription:add', this.bind('addSubscription'))
+				this.layout.regionForm.show(form);
 			},
 
 			reloadForm: function () {
-
-				models.loadFormData()
-					.done(function (formData) {
-
-						var form = new views.SubscriptionFormView({ model: formData });
-						form.on('scripts:subscription:add', api.addSubscription);
-						layoutView.regionForm.show(form);
-					});
+				models.loadFormData().done(this.bind('displayForm'));
 			}
-		};
+		});
 
-		return {
-			start: function () {
-
-				// init layout
-				layoutView = new views.SubscriptionLayout();
-				application.setContentView(layoutView);
-
-				api.reloadForm();
-				api.reloadList();
-			}
-		};
+		return subscriptionList;
 	});
