8e9688dd858f1621c1d8ab945c59c04f4684fbfd
dima117 <dima117a@gmail.com>
2015-10-19 22:18:02 +0300

    - адаптирована страница с прогнозом погоды;

--- ThinkingHome.Plugins.Weather/Resources/js/forecast/forecast.js
+++ ThinkingHome.Plugins.Weather/Resources/js/forecast/forecast.js
@@ -1,29 +1,22 @@
 ﻿define(
-	['app',
+	['lib',
 		'webapp/weather/forecast-model',
 		'webapp/weather/forecast-view'],
-	function (application, models, views) {
+	function (lib, models, views) {
 
-		var api = {
-
-			reload: function () {
-
-				models.loadList()
-					.done(function (collection) {
-
-						var view = new views.WeatherForecastView({
-							collection: collection
-						});
-						
-						application.setContentView(view);
-					});
-			}
-		};
-
-		return {
+		var forecast = lib.common.AppSection.extend({
 			start: function () {
+				models.loadList().done(this.bind('displayForecast'));
+			},
 
-				api.reload();
+			displayForecast: function (collection) {
+				var view = new views.WeatherForecastView({
+					collection: collection
+				});
+
+				this.application.setContentView(view);
 			}
-		};
+		});
+
+		return forecast;
 	});
