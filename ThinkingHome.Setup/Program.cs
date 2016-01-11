d63b17e06a83cb5a818a7180ca72514d25956d47
dima117 <dima117a@gmail.com>
2016-01-11 21:18:17 +0300

    - формирование списка файлов в новом инсталляторе

--- ThinkingHome.Setup/Program.cs
+++ ThinkingHome.Setup/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using WixSharp;
-using File = WixSharp.File;
 
 namespace ThinkingHome.Setup
 {
@@ -10,32 +8,14 @@ namespace ThinkingHome.Setup
 	{
 		static void Main(string[] args)
 		{
-			//var content = BuildContent(args[0]);
-			var content = BuildContent(@"C:\Users\dima117\Documents\Visual Studio 2015\Projects\thinking-home\Setup\dist\service");
-
-			var project = new Project("Thinking Home", content)
+			var project = new Project("Thinking Home", new Dir(@"%ProgramFiles%\ThinkingHome", new Files("*.*")))
 			{
 				Platform = Platform.x64,
-				GUID = new Guid("25F88AAD-CF60-4CA4-ADD3-F01A4F0F0313")
+				GUID = new Guid("25F88AAD-CF60-4CA4-ADD3-F01A4F0F0313"),
+				SourceBaseDir = @"C:\Users\dima117\Documents\Visual Studio 2015\Projects\thinking-home\Setup\dist\service"
 			};
 
-
 			Compiler.BuildMsi(project);
-		}
-
-		static Dir BuildContent(string path)
-		{
-			var allFiles = Directory
-				.GetFiles(path)
-				.Select(file =>
-				{
-					Console.WriteLine(file);
-					return new File(file);
-				})
-				.Cast<WixEntity>()
-				.ToArray();
-
-			return new Dir(@"%ProgramFiles%\ThinkingHome\My Product", allFiles);
 		}
 	}
 }
