a48024ca61bdda6a6e29a4af9538af47a3349d7b
dima117 <dima117a@gmail.com>
2016-01-14 20:59:29 +0300

    установка сервиса в новом инсталляторе

---
+++ ThinkingHome.Service/AutomationServiceInstaller.cs
@@ -0,0 +1,72 @@
+﻿using System.Configuration.Install;
+using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace ThinkingHome.Service
+{
+	[RunInstaller(true)]
+	public class AutomationServiceInstaller : Installer
+	{
+		public AutomationServiceInstaller()
+		{
+			InitializeComponent();
+			serviceInstaller.StartType = ServiceStartMode.Automatic;
+			serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+		}
+
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			serviceProcessInstaller = new ServiceProcessInstaller();
+			serviceInstaller = new ServiceInstaller();
+
+			// 
+			// serviceProcessInstaller
+			// 
+			serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+			serviceProcessInstaller.Password = null;
+			serviceProcessInstaller.Username = null;
+
+			// 
+			// serviceInstaller1
+			// 
+			serviceInstaller.ServiceName = "ThinkingHome.Service";
+			serviceInstaller.DisplayName = "Home automation service";
+			serviceInstaller.Description = "A service that does very interesting things";
+
+			// 
+			// ProjectInstaller
+			// 
+			Installers.AddRange(new Installer[] { serviceProcessInstaller, serviceInstaller});
+		}
+
+		#endregion Component Designer generated code
+
+		private ServiceProcessInstaller serviceProcessInstaller;
+		private ServiceInstaller serviceInstaller;
+	}
+}
--- ThinkingHome.Setup/Program.cs
+++ ThinkingHome.Setup/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.IO;
 using WixSharp;
+using WixSharp.CommonTasks;
+using Microsoft.Deployment.WindowsInstaller;
 
 namespace ThinkingHome.Setup
 {
@@ -8,15 +9,40 @@ namespace ThinkingHome.Setup
 	{
 		static void Main(string[] args)
 		{
-			var project = new Project("Thinking Home", new Dir(@"%ProgramFiles%\ThinkingHome", new Files("*.*")))
+			var project = new Project("Thinking Home", 
+				new Dir(@"%ProgramFiles%\ThinkingHome", new Files("*.*")),
+				new ElevatedManagedAction("InstallService", Return.check, When.After, Step.InstallFiles, Condition.NOT_Installed),
+				new ElevatedManagedAction("UnInstallService", Return.check, When.Before, Step.RemoveFiles, Condition.BeingRemoved))
 			{
-				Platform = Platform.x64,
-				GUID = new Guid("25F88AAD-CF60-4CA4-ADD3-F01A4F0F0313"),
-				LicenceFile = "License.rtf",
-				SourceBaseDir = @"C:\Users\dima117\Documents\Visual Studio 2015\Projects\thinking-home\Setup\dist\service"
+				SourceBaseDir = @"C:\Users\dima117\Documents\Visual Studio 2015\Projects\thinking-home\Setup\dist\service",
+                LicenceFile = "License.rtf",
+                Platform = Platform.x64,
+				GUID = new Guid("25F88AAD-CF60-4CA4-ADD3-F01A4F0F0313")
 			};
 
 			Compiler.BuildMsi(project);
+		}
+	}
+
+	public class CustomActions
+	{
+		[CustomAction]
+		public static ActionResult InstallService(Session session)
+		{
+			return session.HandleErrors(() =>
+			{
+				Tasks.InstallService(session.Property("INSTALLDIR") + "ThinkingHome.Service.exe", true);
+				Tasks.StartService("ThinkingHome.Service", false);
+			});
+		}
+
+		[CustomAction]
+		public static ActionResult UnInstallService(Session session)
+		{
+			return session.HandleErrors(() =>
+			{
+				Tasks.InstallService(session.Property("INSTALLDIR") + "ThinkingHome.Service.exe", false);
+			});
 		}
 	}
 }
